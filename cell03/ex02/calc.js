bc3501270c5d0681021caf4b800b2a382c727820
Mondanai Sophai <68051099@kmitl.ac.th>
2026-09-16 20:59:14 +0700

    Add cell03 exercise 02

---
+++ cell03/ex02/calc.js
@@ -0,0 +1,45 @@
+const calculator = document.getElementById("calculator");
+
+calculator.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const leftValue = document.getElementById("left-number").value;
+    const rightValue = document.getElementById("right-number").value;
+    const operator = document.getElementById("operator").value;
+
+    const left = Number(leftValue);
+    const right = Number(rightValue);
+
+    if (
+        leftValue.trim() === "" ||
+        rightValue.trim() === "" ||
+        !Number.isInteger(left) ||
+        !Number.isInteger(right) ||
+        left < 0 ||
+        right < 0
+    ) {
+        alert("Error :(");
+        return;
+    }
+
+    if ((operator === "/" || operator === "%") && right === 0) {
+        alert("It's over 9000!");
+        console.log("It's over 9000!");
+        return;
+    }
+
+    let result;
+
+    if (operator === "+") result = left + right;
+    else if (operator === "-") result = left - right;
+    else if (operator === "*") result = left * right;
+    else if (operator === "/") result = left / right;
+    else if (operator === "%") result = left % right;
+
+    alert(result);
+    console.log(result);
+});
+
+setInterval(function () {
+    alert("Please, use me...");
+}, 30000);
